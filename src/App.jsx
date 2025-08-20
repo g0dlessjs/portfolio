141c77dda29c61cc6f93dac89eac9d73f2ef242c
g0dlessjs <juangalarce.dev@gmail.com>
2025-08-20 08:29:08 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const app = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl underline">Welcome to my 3d portfolio</h1>
+      </div>
+    </main>
+  );
+};
+
+export default app;
